6da873ff77a4c21871514d5f426fec9d0dcdc17c
anaaf7 <anabeatrizkraener@gmail.com>
2026-03-29 22:11:17 -0300

    Página de login pronta com lógica para espelhamento do fundoPadrao implementada além do uso da biblioteca alpine.js para o icone de mostrar e ocultar senha

--- resources/views/components/layouts/crud.blade.php
+++ resources/views/components/layouts/crud.blade.php
@@ -1,28 +1,34 @@
+{{-- Necessário para quando o fundo precisar ser espelhado --}}
+@props(['espelhar' => false]) 
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acolher</title>
-
-     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-fundo-padrao font-jaldi">
+<body class="font-jaldi">
 
-{{-- Header --}}
-<header class="flex justify-between items-center p-3">
-   <img src="{{asset('imagens/logo-sem-nome.png') }}" alt="Logo do projeto Acolher" class="h-10">
+    {{-- Fundo que leva o parâmetro de espelhamento --}}
+    <div class="fixed inset-0 -z-10 bg-fundo-padrao bg-cover bg-center {{ $espelhar ? '-scale-x-100' : '' }}">
+    </div>
 
-    <a href="" class="flex items-center gap-2 bg-preto text-white px-5 py-2 rounded-full hover:bg-vermelho-escuro transition shadow-md">
+    {{-- Header --}}
+    <header class="relative z-10 flex justify-between items-center p-3">
+        <img src="{{ asset('imagens/logo-sem-nome.png') }}" alt="Logo" class="h-10">
+       
+        <a href="/" class="flex items-center gap-2 bg-preto text-white px-5 py-2 rounded-full hover:bg-vermelho-escuro transition shadow-md">
             <i class="fa-solid fa-user fa-lg"></i>
             <span class="font-jaldi-bold">Entrar</span>
     </a>
-</header>
+    </header>
 
-
-   {{-- Conteúdo da página --}}
-    <main class="min-h-screen relative overflow-hidden flex flex-col items-center justify-center p-4">
+    {{-- Conteúdo --}}
+    <main class="relative z-10 min-h-screen flex flex-col items-center justify-center p-4">
         {{ $slot }}
     </main>
+
 </body>
 </html>
